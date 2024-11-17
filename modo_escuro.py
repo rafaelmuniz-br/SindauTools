ae48c90206d87969f816821af52c4a2c4cac3357
D-Jefferson <jefferson.l1357@gmail.com>
2024-11-17 12:02:08 -0300

    Alteração na organização do menu_escuro

--- modo_escuro.py
+++ modo_escuro.py
@@ -23,7 +23,24 @@ def menu_escuro():
         menu_window.destroy()
         from config import janela
         janela.mainloop()
-    
+        
+    def modo():
+        menu_window.destroy()
+        from modo_claro import menu_claro
+        menu_claro()
+        
+    def sobre():
+        def abrir_github_desenvolvedores():
+            webbrowser.open("https://github.com/D-Jefferson"), webbrowser.open("https://github.com/rafaelmuniz-br")
+        for widget in menu_window.winfo_children():
+            widget.destroy()
+        label_2 = ctk.CTkLabel(menu_window, text= "O SindauTools é um conjunto de ferramentas criado para apoiar a equipe de suporte técnico, adaptando-se constantemente às suas demandas, com o objetivo de aumentar a produtividade, ao acelerar processos encontrados nos trabalhos realizados.",  wraplength=220)
+        label_2.pack(pady=20)
+        btn_1 = ctk.CTkButton(menu_window, text="Desenvolvedores", command=abrir_github_desenvolvedores, width=20, hover_color="gray60")
+        btn_1.pack(pady=0)
+        btn_2 = ctk.CTkButton(menu_window, text="Voltar", command=voltar, width=20)
+        btn_2.pack(pady=20)
+        
     def ferramentas_api():
         for widget in menu_window.winfo_children():
             widget.destroy()
@@ -36,7 +53,6 @@ def menu_escuro():
         btn_3 = ctk.CTkButton(menu_window, text="Voltar", command=voltar, width=20)
         btn_3.pack(pady=80)
         
-
     def ferramentas_geral():
         for widget in menu_window.winfo_children():
             widget.destroy()
@@ -46,65 +62,6 @@ def menu_escuro():
         btn_1.pack(pady=10)
         btn_2 = ctk.CTkButton(menu_window, text="Voltar", command=voltar, width=20)
         btn_2.pack(pady=20)
-
-    def sobre():
-        for widget in menu_window.winfo_children():
-            widget.destroy()
-        label_2 = ctk.CTkLabel(menu_window, text= "O SindauTools é um conjunto de ferramentas criado para apoiar a equipe de suporte técnico, adaptando-se constantemente às suas demandas, com o objetivo de aumentar a produtividade, ao acelerar processos encontrados nos trabalhos realizados.",  wraplength=220)
-        label_2.pack(pady=20)
-        btn_1 = ctk.CTkButton(menu_window, text="Desenvolvedores", command=abrir_github_desenvolvedores, width=20, hover_color="gray60")
-        btn_1.pack(pady=0)
-        btn_2 = ctk.CTkButton(menu_window, text="Voltar", command=voltar, width=20)
-        btn_2.pack(pady=20)
-        
-    def verificar_atualizacoes(url = "https://d-jefferson.github.io/Update/"):
-        try:
-            resposta = requests.head(url)
-            if resposta.status_code < 400:
-                print(f"O link {url} é válido.")
-                webbrowser.open("https://d-jefferson.github.io/Update/")
-                status_label = ctk.CTkLabel(menu_window, text="[ Há atualizações disponiveis ]", font=ctk.CTkFont(size=14), text_color="green")
-                status_label.pack(pady=10)
-            else:
-                print(f"O link {url} é inválido. Código de status: {resposta.status_code}")
-                status_label = ctk.CTkLabel(menu_window, text="[ Não há atualizações disponiveis ]", font=ctk.CTkFont(size=14), text_color="teal")
-                status_label.pack(pady=10)
-        except requests.exceptions.RequestException:
-            print(f"O link {url} é inválido.")
-            
-    def veri():
-        style = ttk.Style()
-        style.theme_use("default")
-        progressbar = ttk.Progressbar(menu_window, orient="horizontal", length=200, mode="indeterminate")
-        progressbar.pack(pady=10)
-        progressbar.start()
-        def parar():
-            progressbar.pack_forget()
-        menu_window.after(5500, verificar_atualizacoes)
-        menu_window.after(5500, parar)
-        
-    def atualizacao():
-        for widget in menu_window.winfo_children():
-            widget.destroy()
-        btn_2 = ctk.CTkButton(menu_window, text="Voltar", command=voltar, width=20,height=3, fg_color="teal", hover_color="darkred",font=ctk.CTkFont(size=9))
-        btn_2.pack(pady=0)
-        label_1 = ctk.CTkLabel(menu_window, text="Novidades - 1.0.3", font=ctk.CTkFont(size=24, weight="bold"), text_color="teal")
-        label_1.pack(pady=10)
-        label_2 = ctk.CTkLabel(menu_window, text= "Melhorias e Ajustes:\n1. Novo visual do menu principal.\n2. Adicionado Des/Vincular Aulas\n3. Melhorado sistema notificação\n4. Aprimorado formatador de CPF", font=ctk.CTkFont(size=14), text_color="gray15")
-        label_2.pack(pady=20)
-        btn_1 = ctk.CTkButton(menu_window, text="Verificar atualizações", command=veri, width=20, fg_color="gray75", hover_color="gray60",text_color="teal", font=ctk.CTkFont(size=15))
-        btn_1.pack(pady=(30,0))
-        status_label = ctk.CTkLabel(menu_window, text="", font=ctk.CTkFont(size=14), text_color="yellow")
-        status_label.pack(pady=10)
-
-    def abrir_github_desenvolvedores():
-        webbrowser.open("https://github.com/D-Jefferson")
-        webbrowser.open("https://github.com/rafaelmuniz-br")
-        
-    def modo():
-        menu_window.destroy()
-        from modo_claro import menu_claro
-        menu_claro()
         
     def menu():
         label = ctk.CTkLabel(menu_window, text="SindauTools", font=ctk.CTkFont(size=20, weight="bold"),corner_radius=15)
@@ -119,7 +76,7 @@ def menu_escuro():
         btn_4.pack(pady=10)
         btn_frame = ctk.CTkFrame(menu_window, fg_color="gray14")
         btn_frame.pack(pady=(135,0))
-        btn_6 = ctk.CTkButton(btn_frame, text="\nBeta 1.0.1", width=50,command=atualizacao, text_color="#103454",fg_color="gray14",hover_color="gray14", font=ctk.CTkFont(size=12, weight="bold"))
+        btn_6 = ctk.CTkButton(btn_frame, text="\nBeta 1.0.1", width=50,command="atualizacao", text_color="#103454",fg_color="gray14",hover_color="gray14", font=ctk.CTkFont(size=12, weight="bold"))
         btn_6.grid(row=0, column=1, padx=(50,0))
         btn_7 = ctk.CTkButton(btn_frame, text="💡 ", width=50,command=modo, hover_color="gray14", fg_color="gray14")
         btn_7.grid(row=0, column=0, padx=(0,50))

4c5a545a2ede98883bc5e12ee41b814b9c04b8bc
D-Jefferson <jefferson.l1357@gmail.com>
2024-11-16 23:50:46 -0300

    Ajuste menu principal, personalização do botão de tema.

--- modo_claro.py
+++ modo_claro.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 import requests
 import webbrowser
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 from formatador_notificacao import abrir_formatador_notificacao
 from formatador_vinculador import abrir_formatador_vinculador
@@ -114,20 +115,22 @@ def menu_claro():
         menu_window.destroy()
         from modo_escuro import menu_escuro
         menu_escuro()
-        
+    
     def menu():
-        label = ctk.CTkLabel(menu_window, text="SindauTools", font=ctk.CTkFont(size=20, weight="bold"),corner_radius=15)
+        label = ctk.CTkLabel(menu_window, text="SindauTools", font=ctk.CTkFont(size=20, weight="bold"),corner_radius=15,fg_color="darkred")
         label.pack(pady=20)
         btn_1 = ctk.CTkButton(menu_window, text="Ferramentas - API", command=ferramentas_api, width=200)
         btn_1.pack(pady=10)
         btn_2 = ctk.CTkButton(menu_window, text="Ferramentas - Geral", command=ferramentas_geral, width=200)
         btn_2.pack(pady=10)
-        btn_3 = ctk.CTkButton(menu_window, text="Alterar Tema", command=modo, width=200,hover_color="gray75")
+        btn_3 = ctk.CTkButton(menu_window, text="Sobre", width=200,command=sobre)
         btn_3.pack(pady=10)
-        btn_5 = ctk.CTkButton(menu_window, text="Sobre", width=200,command=sobre)
-        btn_5.pack(pady=10)
-        btn_6 = ctk.CTkButton(menu_window, text="                                       Beta 1.0.1", width=280,command=atualizacao, font=ctk.CTkFont(size=12, weight="bold"), fg_color="gray35")
-        btn_6.pack(pady=(220,0))
+        btn_frame = ctk.CTkFrame(menu_window, fg_color="gray75")
+        btn_frame.pack(pady=(180,0))
+        btn_6 = ctk.CTkButton(btn_frame, text="Beta 1.0.1", width=50,command=atualizacao, text_color="teal",fg_color="gray75",hover_color="gray75", font=ctk.CTkFont(size=12, weight="bold"))
+        btn_6.grid(row=0, column=1, padx=(50,0))
+        btn_7 = ctk.CTkButton(btn_frame, text="Dark", width=50,height=15,command=modo, text_color="white",hover_color="gray35", font=ctk.CTkFont(size=12, weight="bold"), corner_radius=70,fg_color="gray15")
+        btn_7.grid(row=0, column=0, padx=(0,50))
     menu()
 
     menu_window.mainloop()
